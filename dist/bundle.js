308ad9fac9916020f5eae4509f186cddb7803998
adamteddychang <adamchang0725@gmail.com>
2021-11-16 09:09:10 +0100

    Completed footer

--- dist/bundle.js
+++ dist/bundle.js
@@ -116,7 +116,7 @@ eval("\n\n/* istanbul ignore next  */\nfunction styleTagTransform(css, styleElem
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n\n\nalert('Hello There');\n\n//# sourceURL=webpack://webpack-demo/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n\n\nconst sampleFunc = () => {\n  console.log('Hello There!');\n};\n\nsampleFunc();\n\n//# sourceURL=webpack://webpack-demo/./src/index.js?");
 
 /***/ })
 
