704829986f1b55d5ab1da95fa56d8a4f19905282
Taher <ttaherhagui@gmail.com>
2021-11-17 14:04:56 +0100

    Display number of likes automatically in the page.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import LOGO from './assets/Images/logo.jpg';
 import LIKE from './assets/Images/like.png';
 import COMMENT from './assets/Images/comment.png';
 import { consumeMeals, counterHomeItems } from './consume_meals.js';
+import { getLikes, postLikes } from './manage-likes.js';
 // declarate variables //
 const header = document.querySelector('header');
 const cards = document.querySelector('.items');
@@ -18,14 +19,35 @@ const load = async () => {
   const itemsArray = res.meals;
   let i = 0;
   const itemsIndex = {};
+  const resp = await getLikes();
+  let likes = [];
+  if ((!resp.status) || (resp.status !== 200)) {
+    likes = resp.json();
+  }
   while (!counterHomeItems(cards, 6)) {
     const item = itemsArray[i];
     if ((!!item.strMeal) && (item.strMeal !== '') && (!!item.strMealThumb)) {
       itemsIndex[i] = item.idMeal;
+      let j = likes.length;
+      let nbrLikes = 0;
+      if (j > 0) {
+        for (let k = 0; k < likes.length; k += 1) {
+          const itemObj = likes[k];
+          if (itemObj.item_id === itemsIndex[i]) {
+            nbrLikes = itemObj.likes;
+            break;
+          }
+          j -= 1;
+        }
+      }
+      if (j === 0) {
+        nbrLikes = 0;
+        postLikes(itemsIndex[i], nbrLikes);
+      }
       const div = document.createElement('div');
       div.innerHTML = `<img src=${item.strMealThumb} alt = 'image item' class='image-item'>`
       + `<div class='item-data'><p>${item.strMeal}</p><img src=${LIKE} alt='like icon' class='like'></div>`
-      + '<p class=\'likes-nbr\'><span>0</span> likes</p>'
+      + `<p class='likes-nbr'><span>${nbrLikes}</span> likes</p>`
       + `<button class="btn"> <span>Comments</span> <div class='comment-icon'><img src=${COMMENT} ></div></button>`;
       div.className = 'card';
       div.style.gridColumn = `${(i % 3) + 1}/ span 1`;
