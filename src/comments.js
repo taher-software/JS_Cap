fef416b8e122f42520ea298006c9dc1e1b0f5e52
adamteddychang <adamchang0725@gmail.com>
2021-11-18 13:43:23 +0100

    fixed linters

--- src/comments.js
+++ src/comments.js
@@ -3,7 +3,7 @@ const getComm = async () => {
   const comUl = document.querySelector('#comment_div');
   const id = subBtn.getAttribute('data');
   const comCount = document.querySelector('#com_count');
-  const toggle = document.querySelector('#togBtn')
+  const toggle = document.querySelector('#togBtn');
   const get = () => fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/sNKLeXAeMyPD4Nokxkg0/comments?item_id=${id}`)
     .then((res) => res.json());
   const mealCom = await get();
@@ -11,43 +11,43 @@ const getComm = async () => {
   comUl.innerHTML = '';
   let dis = false;
   mealCom.reverse();
-  const displayAllComms = () =>{
-    if (mealCom.length > 0) {    
-    mealCom.forEach((e) => {      
-      const comLi = document.createElement('li');
-      comLi.className = 'comment';
-      comLi.innerHTML = `
+  const displayAllComms = () => {
+    if (mealCom.length > 0) {
+      mealCom.forEach((e) => {
+        const comLi = document.createElement('li');
+        comLi.className = 'comment';
+        comLi.innerHTML = `
       <span class="bold">${e.creation_date} ${e.username}</span>: ${e.comment}
       `;
-      comUl.appendChild(comLi);      
-    }); 
+        comUl.appendChild(comLi);
+      });
+    }
   };
-}
-const display6Comms = () => {    
-    let newArr = mealCom.slice(0,6)
-    newArr.forEach((e) => {      
+  const display6Comms = () => {
+    const newArr = mealCom.slice(0, 6);
+    newArr.forEach((e) => {
       const comLi = document.createElement('li');
       comLi.className = 'comment';
       comLi.innerHTML = `
       <span class="bold">${e.creation_date} ${e.username}</span>: ${e.comment}
       `;
-      comUl.appendChild(comLi);      
-    }); 
-}
+      comUl.appendChild(comLi);
+    });
+  };
 
-displayAllComms();
+  displayAllComms();
 
-toggle.addEventListener ('click', ()=>{
-   if (dis===true){
-    comUl.innerHTML = "";
-    displayAllComms();
-    dis = false;
-  } else if (dis === false) {
-    comUl.innerHTML = "";
-    display6Comms();
-    dis = true;    
-  }
-})
+  toggle.addEventListener('click', () => {
+    if (dis === true) {
+      comUl.innerHTML = '';
+      displayAllComms();
+      dis = false;
+    } else if (dis === false) {
+      comUl.innerHTML = '';
+      display6Comms();
+      dis = true;
+    }
+  });
 };
 
 const postComm = () => {
