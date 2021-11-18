c9356d508e6fc27391b289a2fd37a637d6073c19
Taher <ttaherhagui@gmail.com>
2021-11-18 11:33:04 +0100

    mock the content of html container.

--- src/counter.test.js
+++ src/counter.test.js
@@ -1,1 +1,9 @@
 import {default as counterItem} from './counter.test.js';
+test('test Number of elements returned by counterItem', ()=>{
+  document.body.innerHTML = '<ul>'
+  + '<li>'
+  + '<li>'
+  + '<li>'
+  + '<li>'
+  + '</ul>';
+})
